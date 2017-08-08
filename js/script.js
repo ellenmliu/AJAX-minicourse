ab637bfdaec1025407df5169d92066e2cf06ee11
Ellen Liu <ellen.m.liu@gmail.com>
2017-08-08 11:14:41 -0700

    Add related articles using AJAX from NYTimes API

--- js/script.js
+++ js/script.js
@@ -20,6 +20,26 @@ function loadData() {
     $greeting.text = ("Do you want to live in " + loc + "?");
     $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+loc+'">');
 
+    var prefix = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    var param = "?api-key=bbeca2d23223415ba3f565922deceb91"
+
+    // Getting the articles related to
+    $.getJSON(prefix, {
+      'api-key': 'bbeca2d23223415ba3f565922deceb91',
+      'q': city,
+      'format': 'json'
+    }).done(function(data){
+      $nytHeaderElem.text('New York Times Articles about ' + city);
+      $.each(data.response.docs, function(i, doc){
+        var title = doc.headline.main;
+        var art_url = doc.web_url;
+        var snippet = doc.snippet;
+        $('#nytimes-articles').append('<li><a href="' +
+          art_url + '">' + title +
+          '</a><p>' + snippet + '</p></li>');
+      })
+    });
+
     return false;
 };
 
